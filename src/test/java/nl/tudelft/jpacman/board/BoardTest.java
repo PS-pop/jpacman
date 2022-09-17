5489d8acc4492ebe54a177f22d6e3cb17ea54141
PS-pop <pathty.s10@gmail.com>
2022-09-17 16:37:27 +0700

    AssertionTest

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,30 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+    private Square[][] grid;
+    private Board board;
+
+    private final Square square = new BasicSquare();
+
+    @Test
+    void isBoardValid (){
+        grid = new Square[][]{
+            { square,square}
+        };
+        board = new Board(grid);
+        assertThat(board.invariant()).isEqualTo(true);
+    }
+    @Test
+    void testSquareAt() {
+        grid = new Square[][]{
+            {square,null}
+        };
+        board = new Board(grid);
+        assertThat(board.squareAt(0, 0)).isEqualTo(square);
+    }
+}
